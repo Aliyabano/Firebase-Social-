1b30802e7e4bf96bf773484a61bf0fbeb8a65870
cedcoss <aliyabano@magenative.com>
2022-03-02 15:27:14 +0530

    Firebase

--- app/src/main/java/com/example/usingfirebase/MainActivity.kt
+++ app/src/main/java/com/example/usingfirebase/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.usingfirebase
 
+import android.app.Instrumentation
+import android.app.backup.BackupManager
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.login.LoginResult
+import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FacebookAuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -14,11 +25,15 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+   lateinit var callBackManager : CallbackManager
+    private var EMAIL = "email"
+
     val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        JavaHashKey.printHashKey(this)
 
         forgotPassword.setOnClickListener {
             startActivity(Intent(this,ResetPassword::class.java))
@@ -67,6 +82,39 @@ class MainActivity : AppCompatActivity() {
         else{
             true
         }
+        login_button.setOnClickListener {
+            login_button.setReadPermissions(listOf(EMAIL))
+            callBackManager = CallbackManager.Factory.create()
+            login_button.setReadPermissions(listOf("email"))
 
+            login_button.registerCallback(callBackManager, object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+                    Log.d(TAG, "onCancel: Cancel")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "onError: Error")
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    val request = GraphRequest.newMeRequest(
+                        result?.accessToken
+                    ) { obj, response ->
+                        try {
+                            if (obj!!.has("id"))
+                                Log.d(TAG, "onSuccess: fbObject $obj")
+                        } catch (e: Exception) {
+                        }
+                    }
+                }
+
+            })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callBackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
+
